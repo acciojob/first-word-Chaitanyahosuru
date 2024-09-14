853dbb82c471e5f3ec8fb20f77368cd941919839
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-14 12:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function firstWord(str) {
-  // your code here
-
-    if (str.length === 0) {
-        return '';
-    }
-    
+    // Find the index of the first space
     let spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
     if (spaceIndex === -1) {
         return str;
     }
     
+    // Otherwise, return the substring up to the first space
     return str.substring(0, spaceIndex);
 }
 
 
 
-// Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(str));
