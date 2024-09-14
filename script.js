4c08185e93bc03c62ab869342142371711e1c272
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-14 11:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function firstWord(s) {
   // your code here
+	
+function firstWord(str) {
+
+    if (str.length === 0) {
+        return '';
+    }
+    
+    let spaceIndex = str.indexOf(' ');
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    return str.substring(0, spaceIndex);
 }
+
+}
+
 
 // Do not change the code below
 
