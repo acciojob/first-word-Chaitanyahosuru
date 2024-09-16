06dc5ce7dac8516b4d8e3e09c541b04c9e3a86ef
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-16 07:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function firstWord(str) {
     // Find the index of the first space
-    let spaceIndex = str.indexOf(' ');
-    
-    // If no space is found, return the entire string
-    if (spaceIndex === -1) {
-        return str;
-    }
-    
-    // Otherwise, return the substring up to the first space
-    return str.substring(0, spaceIndex);
+   s = s.trim();
+ // Find the index of the first space
+ const spaceIndex = s.indexOf(' ');
+ // If no space is found, return the entire string
+ if (spaceIndex === -1) {
+ return s;
+ }
+ // Return the substring from the start of the string to the first space
+ return s.substring(0, spaceIndex);
+
 }
 
 
